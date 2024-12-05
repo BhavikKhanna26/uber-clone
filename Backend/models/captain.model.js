abac7fe3e6feb13f2b711bcfcb6ae4891f2f29ab
BhavikK26 <110221809+BhavikK26@users.noreply.github.com>
2024-12-05 22:50:18 +0530

    captain model added

---
+++ Backend/models/captain.model.js
@@ -0,0 +1,86 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+const captain = new mongoose.Schema({
+    fullname : {
+        firstname : {
+            type : String,
+            required : true,
+            minlength : [3, 'Firstname msut be 3 chars or long'],
+        },
+        lastname : {
+            type : String,
+            minlength : [3, 'Lastname msut be 3 chars or long'],
+        },
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        match : [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
+    },
+    password : {
+        type : String,
+        required : true,
+        select : false,
+    },
+    socketId : {
+        type : String,
+    },
+    status : {
+        type : String,
+        enum : ['active', 'inactive'],
+        default : 'inactive'
+    },
+    vehicle : {
+        color : {
+            type : String,
+            required : true,
+            minlength : [3, 'Color must be 3 chars or long'],
+        },
+        plate : {
+            type : String,
+            required : true,
+            minlength : [3, 'Plate must be 3 chars or long'],
+        },
+        capacity : {
+            type : Number,
+            required : true,
+            min : [1, 'Capacity must be at least 1'],
+        },
+        vehicleType : {
+            type : String,
+            required : true,
+            enum : ['motorcycle', 'car', 'auto'],
+        }
+    },
+    location : {
+        lat : {
+            type : Number,
+        },
+        lng : {
+            type : Number
+        }
+    }
+});
+
+captainSchema.models.generateAuthToken = function () {
+    const token = jwt.sign({_id : this._id}, process.env.JWT_SECRET, {expiresIn : '1d'});
+    return token;
+}
+
+captainSchema.methods.comparePassowrd = async function (password) {
+    const isMatch = await bcrypt.compare(password, this.password);
+    return isMatch;
+}
+
+captainSchema.statics.hashPassword = async function (password) {
+    return bcrypt.hash(password, 10);
+}
+
+const captainModel = mongoose.model('captain', captain);
+
+module.exports = captainModel;
---
+++ Backend/routes/captain.routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const { body } = require("express-validator");
+
+
+router.post('/register', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('fullname.firstname').isLength({min : 3}).withMessage('First name be at least 3 chars long'),
+    body('password').isLength({min : 6}).withMessage('Password must be atleast 6 chars long'),
+    body('vehicle.color').isLength({min : 3}).withMessage('Vehicle color be at least 3 chars long'),  
+    body('vehicle.plate').isLength({min : 3}).withMessage('Plate be at least 3 chars long'),
+    body('vehicle.capacity').isLength({min : 3}).withMessage('Capacity be at least 3 chars long'),
+    body('vehicle.vehicleType').isLength({min : 3}).withMessage('Vehicle type be at least 3 chars long')
+]
+)
+
+
+module.exports = router;
